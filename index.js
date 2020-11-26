9e7d1984f4b2f97b8c0cbc58493d82bc7a8009de
Russell <russellcape98@gmail.com>
2020-11-26 16:56:46 -0500

    Making internal requests... externals require billing info

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-/**
- * Responds to any HTTP request.
- *
- * @param {!express:Request} req HTTP request context.
- * @param {!express:Response} res HTTP response context.
- */
-exports.helloWorld = (req, res) => {
-    let message = req.query.message || req.body.message || 'Hello World!';
-    res.status(200).send(message);
-  };
+// exports.myFunction = (req, res) => {
+
+//   let message = {
+//     msg: "Hello, I am a JSON response!"
+//   };
+
+//   res.status(200).send(message);
+
+// };
+
+
+const fetch = require('node-fetch');
+
+
+function getUrlForUser(username) {
+  return `https://api.github.users/${username}`
+}
+
+exports.myFunction = async (req, res) => {
+  const username = req.query.username || 'russellcape';
+
+  try {
+    const response = await fetch(getUrlForUser(username));
+    const responseObject = await response.json();
+    const avatarUrl = responseObject.avatar_url;
+    res.redirect(avatarUrl);
+  } catch(err) {
+    res.status(501).send(err.message);
+  }
+};
